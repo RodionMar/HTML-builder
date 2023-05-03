03faf9f71015f262405b152ffa6518aa937e62a3
RodionMar <rodionmarmura@gmail.com>
2023-05-03 11:22:32 +0300

    fix: complete first task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
 const path = require('node:path');
 const { stdout } = process;
+
 const pathToText = path.join('./', '01-read-file/', 'text.txt');
+const readerText = fs.createReadStream(pathToText);
 
-fs.readFile(pathToText, 'utf-8', function(error, textContent) {
-  if (error) {throw error;} else {stdout.write(textContent);}
+readerText.on('data', function (textContent) {
+  stdout.write(textContent);
 });
-
