a6445d0e148a3ad31abeb7c4995073bd74e91825
RodionMar <rodionmarmura@gmail.com>
2023-05-04 09:00:14 +0300

    feat: complete second task

---
+++ 02-write-file/index.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const path = require('node:path');
+const process = require('node:process');
+const { stdin, stdout} = process;
+const pathToText = path.join('./', '02-write-file/', 'text.txt');
+
+fs.writeFile(pathToText, '', function(error) {
+  if(error) throw error;
+  else{stdout.write('Приветствую! Вводите всё, что вздумается:D\n');}
+});
+
+process.on('exit', () => {
+  stdout.write('Пока! Успехов в обучении!');
+});
+
+process.on('SIGINT', () => {
+  process.exit();
+});
+
+stdin.on('data', data => {
+  if (data.toString().trim() === 'exit') {
+    process.exit();
+  }
+  fs.appendFile(pathToText, data, err => {
+    if (err) throw err;
+  });});
