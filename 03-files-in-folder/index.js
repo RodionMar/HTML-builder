e1dd8a7f28f03d0b95981371f6d407f222bcc0af
RodionMar <rodionmarmura@gmail.com>
2023-05-04 23:10:42 +0300

    feat: complete third task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('node:path');
+const { stdout } = process;
+
+const pathToFolder = path.join(__dirname, 'secret-folder');
+
+fs.readdir(pathToFolder, {withFileTypes: true}, (err, files) => {
+  if(err) throw err;
+  files.forEach(file => {
+    if(!file.isDirectory()) {
+      const newPathToFiles = path.join(pathToFolder, file.name);
+      const extensionInFile = path.extname(newPathToFiles);
+      const removePeriodFromExtension =  extensionInFile.replace('.', '');
+      const removeExtensionFromFileName = file.name.replace(extensionInFile, '');
+      fs.stat(newPathToFiles, function(err, stats) {
+        const moveSizeToKb = stats.size / 1000;
+        stdout.write(`${removeExtensionFromFileName} - ${removePeriodFromExtension} - ${moveSizeToKb}kb\n`);
+      });
+    }
+  });
+});
+
+
