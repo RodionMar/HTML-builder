5b5726579855b6023a5db94f2c6264d283d0d5b6
RodionMar <rodionmarmura@gmail.com>
2023-05-05 22:22:34 +0300

    feat: complete fourth task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,45 @@
+const fs = require('fs');
+const path = require('node:path');
 
+const pathToFileCopy = path.join(__dirname, 'files-copy');
+const pathToOriginalFile = path.join(__dirname, 'files');
+
+fs.rmdir(pathToFileCopy, {recursive: true}, () => {
+  fs.mkdir(pathToFileCopy, {recursive: true}, (err) => {
+    if(err) throw err;
+    fs.readdir(pathToOriginalFile, (err, files) => {
+      for (let i = 0; i < files.length; i++) {
+        const filePath = path.join(__dirname, 'files', files[i]);
+        const copyFilePath = path.join(__dirname, 'files-copy', files[i]);
+        fs.copyFile(filePath, copyFilePath,  (err) => {
+          if(err) throw err;
+        });
+      }
+    });
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// files.forEach(file => {
+         
+//    const filePath = path.join(__dirname, 'files', file)
+//    const copyFilePath = path.join(__dirname, 'files-copy', file)
+//    const copyFileContent = fs.copyFile(filePath, copyFilePath, err => {
+//       if(err) throw err
+//    })
+// })
