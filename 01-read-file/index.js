00d842b28d62c36d7e02bcd09ff9940f0a2b676c
RodionMar <rodionmarmura@gmail.com>
2023-05-08 20:18:25 +0300

    fix: change pathToText

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('node:path');
 const { stdout } = process;
 
-const pathToText = path.join('./', '01-read-file/', 'text.txt');
+const pathToText = path.join(__dirname, 'text.txt');
 const readerText = fs.createReadStream(pathToText);
 
 readerText.on('data', function (textContent) {
